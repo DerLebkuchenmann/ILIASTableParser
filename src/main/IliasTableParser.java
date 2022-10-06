f280f4fbbbcf5170dbcd497aeea2272a7bc49582
DerLebkuchenmann <dh@wagner-hahn.de>
2022-10-06 16:59:52 +0200

    Added app name for MacOS

--- src/main/IliasTableParser.java
+++ src/main/IliasTableParser.java
@@ -4,6 +4,10 @@ import gui.TPGui;
 
 public class IliasTableParser {
 	public static void main(String[] args) {
+		if (System.getProperty("os.name") == "Mac OS X") {
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+			System.setProperty("com.apple.mrj.application.apple.menu.about.name", "ILIAS Table Parser");
+		}
 		TPGui gui = new TPGui();
 		gui.setVisible(true);
 	}
