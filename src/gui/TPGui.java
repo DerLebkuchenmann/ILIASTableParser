cd0b31117780db30370f4bee4cafec8a2c15f3b9
DerLebkuchenmann <17740555+DerLebkuchenmann@users.noreply.github.com>
2023-06-18 20:26:03 +0200

    Initial running version

---
+++ src/gui/TPGui.java
@@ -0,0 +1,78 @@
+package gui;
+
+import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import input.HTMLReader;
+import output.CSVWriter;
+
+public class TPGui extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private Container c;
+	private JPanel base;
+	private JFileChooser fileChooser;
+	private JButton openFile;
+	private HTMLReader reader;
+	private CSVWriter writer;
+	private File output;
+
+	public TPGui() {
+		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		createWindow();
+		reader = new HTMLReader();
+		writer = new CSVWriter();
+
+	}
+
+	private void createWindow() {
+		c = this.getContentPane();
+		base = new JPanel();
+		fileChooser = new JFileChooser();
+		openFile = new JButton("Open HTML file");
+		c.add(base);
+		base.add(openFile);
+		setActions();
+	}
+
+	private void setActions() {
+		openFile.addActionListener(e -> {
+			int returnVal = fileChooser.showOpenDialog(this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fileChooser.getSelectedFile();
+				try {
+					output = new File(file.getParentFile(), "results.csv");
+					boolean success = writer.writeToCSV(reader.getValues(file), output);
+					if (success) {
+						JOptionPane.showMessageDialog(this, output.getAbsolutePath() + " was successfully created",
+								"Success", JOptionPane.INFORMATION_MESSAGE);
+					}
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(this, e1.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		setWindowClosingOps();
+		this.pack();
+
+	}
+
+	private void setWindowClosingOps() {
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+				writer.closeWriter();
+			}
+		});
+
+	}
+}
---
+++ src/output/CSVWriter.java
@@ -0,0 +1,43 @@
+package output;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
+public class CSVWriter {
+	private BufferedWriter bufWr;
+	private String separator = ";";
+
+	public boolean writeToCSV(LinkedList<LinkedList<String>> values, File file) throws IOException {
+		bufWr = new BufferedWriter(new FileWriter(file));
+		int count;
+		for (LinkedList<String> user : values) {
+			count = 0;
+			for (String value : user) {
+				if (value.length() == 0) {
+					continue;
+				}
+				bufWr.write(value);
+				if (count < user.size()) {
+					bufWr.write(separator);
+				}
+				count++;
+			}
+			bufWr.newLine();
+		}
+		closeWriter();
+		return true;
+	}
+
+	public void closeWriter() {
+		try {
+			bufWr.close();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, e.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+}
